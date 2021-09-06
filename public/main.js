4e69ead8a928d6457ae5178713c6ba33207b7de9
carob16 <cebbe95@gmail.com>
2021-09-06 20:26:25 +0200

    removed slider from page

--- public/main.js
+++ public/main.js
@@ -6,29 +6,6 @@ window.addEventListener('deviceorientation', handleOrientation, true);
 window.addEventListener('devicemotion', handleMotion, true);
 
 // ----------------FUNCTIONS-------------------------------
-var slider = document.getElementById('RotationX');
-xPosRot = (slider.value * Math.PI) / 180;
-
-function handleOrientation(event) {
-  var x = event.beta; // In degree in the range [-180,180)
-  var y = event.gamma; // In degree in the range [-90,90)
-  var z = event.alpha; // In degree in the range [-90,90)
-  let DegToRad = Math.PI / 180;
-  //print text
-  outputTextElement('xPosRot', `xPosRot : ${fixedNumber(x)}\n`);
-  outputTextElement('yPosRot', `yPosRot: ${fixedNumber(y)}\n`);
-  outputTextElement('zPosRot', `zPosRot: ${fixedNumber(z)}\n`);
-
-  y = y * DegToRad;
-  x = x * DegToRad;
-  z = z * DegToRad;
-
-  xPosRot = x;
-  yPosRot = y;
-  zPosRot = z;
-
-  //updatePosition();
-}
 
 function handleMotion(event) {
   var xAcc = event.acceleration.x;

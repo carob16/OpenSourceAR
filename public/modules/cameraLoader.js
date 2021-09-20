dd0dc125fc9870fadd487537f76065740e83293b
carob16 <cebbe95@gmail.com>
2021-09-20 12:37:00 +0200

    Trying grayscale 100% on livestream

--- public/modules/cameraLoader.js
+++ public/modules/cameraLoader.js
@@ -4,7 +4,7 @@ if (navigator.mediaDevices.getUserMedia) {
     .getUserMedia({ video: { facingMode: { ideal: 'environment' } } })
     .then(function (stream) {
       video.srcObject = stream;
-      video.style.filter = 'contrast 150%';
+      video.style.filter = 'grayscale(100%)';
     })
     .catch(function (err) {
       var videoElement = document.getElementById('video-background');

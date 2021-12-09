4165c3f23085d39162878ce46c36bf77aab6a2a1
carob16 <carob16@uia.no>
2021-12-09 11:37:28 +0100

    permission and connection-status added to accelerometer og gyroscope

--- public/main.js
+++ public/main.js
@@ -1,11 +1,67 @@
 //import {*} from "../js/sensorDetection.js"
 
 //-------------------EVENTS-------------------------------------
+// var cornerMat = new cv.Mat();
+let accelerometer = null;
+let gyroscope = null;
 
-window.addEventListener('deviceorientation', handleOrientation, true);
-window.addEventListener('devicemotion', handleMotion, true);
+try{
+//window.addEventListener('deviceorientation', handleOrientation, true);
+//window.addEventListener('devicemotion', handleMotion, true);
+accelerometer = new Accelerometer({ referenceFrame: 'device' });
+gyroscope = new Gyroscope();
+    accelerometer.addEventListener('error', event => {
+        // Handle runtime errors.
+        if (event.error.name === 'NotAllowedError') {
+          outputTextElement('sensorstatus-acc','Please check your sensor-permissions in settings before reloading the page');
+            // Branch to code for requesting permission.
+        } else if (event.error.name === 'NotReadableError' ) {
+            console.log('Cannot connect to the accelerometer.');
+            outputTextElement('sensorstatus-acc','Cannot connect to the accelerometer.');
+        }
+    });
+    gyroscope.addEventListener('error', event => {
+      // Handle runtime errors.
+      if (event.error.name === 'NotAllowedError') {
+        outputTextElement('sensorstatus-gyro','Please check your sensor-permissions in settings before reloading the page');
+      } else if (event.error.name === 'NotReadableError' ) {
+          console.log('Cannot connect to the gyroscope sensor.');
+          outputTextElement('sensorstatus-gyro','Cannot connect to the gyroscope.');
+      }
+  });
+
+    accelerometer.addEventListener('devicemotion', handleMotion, true);
+   gyroscope.addEventListener('deviceorientation', handleOrientation, true);
+    accelerometer.start();
+    gyroscope.start();
+}catch(error){
+  if (error.name === 'SecurityError') {
+    // See the note above about feature policy.
+    console.log('Sensor construction was blocked by a feature policy.');
+} else if (error.name === 'ReferenceError') {
+    console.log('Sensor is not supported by the User Agent.');
+} else {
+    throw error;
+}
+}
+
+var edgeDetectionCanvasDiv = document.getElementById('output'); 
+//edgeDetectionCanvasDiv.addEventListener('mousemove', function getMousePosition(e){console.log(e.clientX, e.clientY);}, true);
+edgeDetectionCanvasDiv.addEventListener('click', function getSquarePosition(e){
+  detectCorner(e.clientX, e.clientY);}, true);
+// document.getElementById("slider_1").addEventListener("change", "onSliderChange()");
+// document.getElementById("slider_2").addEventListener("change", "onSliderChange()");
+// document.getElementById("slider_3").addEventListener("change", "onSliderChange()");
 
 // ----------------FUNCTIONS-------------------------------
+
+function detectCorner(eX,eY){
+clickX = eX;
+clickY = eY;
+//edgeDetection();
+  
+}
+
 
 function outputTextElement(name, Text) {
 
@@ -47,3 +103,4 @@ function downloadCSV(filename) {
 
   downloadLink.click();
 }
+

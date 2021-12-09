e56bb6bb4a21e9a55abbf2870bf7f3f0bfb9d754
carob16 <carob16@uia.no>
2021-12-09 11:57:12 +0100

    Reimplemented deviceOrientation and deviceMotion because of compability for browsers

--- public/main.js
+++ public/main.js
@@ -1,49 +1,16 @@
-//import {*} from "../js/sensorDetection.js"
 
 //-------------------EVENTS-------------------------------------
-// var cornerMat = new cv.Mat();
-let accelerometer = null;
-let gyroscope = null;
 
-try{
-//window.addEventListener('deviceorientation', handleOrientation, true);
-//window.addEventListener('devicemotion', handleMotion, true);
-accelerometer = new Accelerometer({ referenceFrame: 'device' });
-gyroscope = new Gyroscope();
-    accelerometer.addEventListener('error', event => {
-        // Handle runtime errors.
-        if (event.error.name === 'NotAllowedError') {
-          outputTextElement('sensorstatus-acc','Please check your sensor-permissions in settings before reloading the page');
-            // Branch to code for requesting permission.
-        } else if (event.error.name === 'NotReadableError' ) {
-            console.log('Cannot connect to the accelerometer.');
-            outputTextElement('sensorstatus-acc','Cannot connect to the accelerometer.');
-        }
-    });
-    gyroscope.addEventListener('error', event => {
-      // Handle runtime errors.
-      if (event.error.name === 'NotAllowedError') {
-        outputTextElement('sensorstatus-gyro','Please check your sensor-permissions in settings before reloading the page');
-      } else if (event.error.name === 'NotReadableError' ) {
-          console.log('Cannot connect to the gyroscope sensor.');
-          outputTextElement('sensorstatus-gyro','Cannot connect to the gyroscope.');
-      }
-  });
-
-    accelerometer.addEventListener('reading', handleMotion, true);
-   gyroscope.addEventListener('reading', handleOrientation, true);
-    accelerometer.start();
-    gyroscope.start();
-}catch(error){
-  if (error.name === 'SecurityError') {
-    // See the note above about feature policy.
-    console.log('Sensor construction was blocked by a feature policy.');
-} else if (error.name === 'ReferenceError') {
-    console.log('Sensor is not supported by the User Agent.');
-} else {
-    throw error;
-}
-}
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', handleOrientation, true);
+  }else{
+    outputTextElement('sensorstatus-acc','Cannot read DeviceOrientation-data. Please check your sensor-permissions in settings before reloading the page');
+  }
+  if(window.DeviceMotionEvent){
+    window.addEventListener('devicemotion', handleMotion, true);
+  }else{
+    outputTextElement('sensorstatus-acc','Cannot read DeviceMotion-data. Please check your sensor-permissions in settings before reloading the page');
+  }
 
 var edgeDetectionCanvasDiv = document.getElementById('output'); 
 //edgeDetectionCanvasDiv.addEventListener('mousemove', function getMousePosition(e){console.log(e.clientX, e.clientY);}, true);
